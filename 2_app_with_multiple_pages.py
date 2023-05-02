4399afb4192db3818330caa64e9b287e86d00a41
Ed Denham <ed.denham@gmail.com>
2023-05-02 04:03:48 +0000

    Initial commit

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from app_pages.multi_page import MultiPage
 
+from app_pages.page1 import page1_body
+from app_pages.page2 import page2_body
+
+app = MultiPage(app_name='Rock solid app discovery!')
+app.app_page("Page The First", page1_body)
+app.app_page("Page The Second", page2_body)
+
+app.run()
--- 3_calculator.py
+++ 3_calculator.py
@@ -1,1 +1,8 @@
+import streamlit as st 
+from app_pages.multi_page import MultiPage
+from app_pages.page_calculator import calculator_body
+    
+app = MultiPage(app_name='My Calculator')
 
+app.app_page("Calculator", calculator_body)
+app.run()
